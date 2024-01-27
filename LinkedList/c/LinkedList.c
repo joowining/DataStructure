84cf9f8302e0a52b5093d236df9671c6405c2b26
JoowonKim <99549623+joowining@users.noreply.github.com>
2024-01-27 22:25:21 +0900

    List - LinkedList in C

---
+++ LinkedList/c/LinkedList.c
@@ -0,0 +1,134 @@
+#include "LinkedList.h"
+
+// 노드생성 
+Node* SLL_CreateNode(ElementType NewData)
+{
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+
+    NewNode->Data = NewData;
+    NewNode->NextNode = NULL;
+
+    return NewNode;
+}
+
+//노드 소멸 
+void SLL_DestroyNode(Node* Node)
+{
+    free(Node);
+}
+
+void SLL_DestroyAllNode(Node** List)
+{
+    Node* Current;
+    Node* Next;
+
+    Current = *List;
+    Next = NULL;
+    while(Current != Null)
+    {
+      Next = Current->NextNode;
+      SLL_DestroyNode(Current);
+      Node* Current = Next;  
+    }
+}
+
+//노드 추가
+void SLL_AppendNode(Node** Head, Node* NewNode)
+{
+    // 헤드 노드가 NULL일시 입력 노드를 Head로 
+    if((*Head) == NULL)
+    {
+        *Head = NewNode;
+    }
+    else 
+    {
+        Node* Tail = (*Head);
+        while(Tail->NextNode != Null)
+        {
+            Tail = Tail->NextNode;
+        }
+        Tail->NextNode = NewNode;
+    }
+}
+
+//노드 삽입
+void SLL_InsertAfter(Node* Current, Node* NewNode)
+{
+    NewNode->NextNode = Current->NextNode;
+    Current->NextNode = NewNode;
+}
+
+void SLL_INsertBefore(Node** Head,Node* Current, Node* NewNode)
+{
+    Node* BeforeNode = *Head;
+    
+    while(BeforeNode->NextNode != Current)
+    {
+        BeforeNode = BeforeNode->NextNode;
+    }    
+    BeforeNode->NextNode = NewNode;
+    NewNode->NextNode = Current;
+}
+
+void SLL_InsertNewHead(Node** Head, Node* NewHead)
+{
+    if(*Head == NULL)
+    {
+        (*Head) = NewHead;
+    }
+    else 
+    {
+        NewHead->NextNode = (*Head);
+        (*Head) = NewHead;
+    }
+}
+
+//노드 제거
+void SLL_RemoveNode(Node** Head, Node* Remove)
+{
+    if(*Head == Remove)
+    {
+        *Head = Remove->NextNode;
+    }
+    else
+    {
+        Node* Current = *Head;
+        while(Current !=NULL ** Current->NextNode !=Remove)
+        {
+            Current = Current->NextNode;
+        }
+        if(Current != NULL)
+        {
+            current->NextNode = Remove->NextNode;
+        }
+    }
+}
+
+//노드 탐색
+Node* SLL_GetNodeAt(Node* Head, int Location)
+{
+    Node* Current = Head;
+
+    while(Current != NULL && (--Location) >=0)
+    {
+        Current = Current->NextNode;
+    }
+    return Current;
+}
+
+//노드 수 세기
+int SLL_GetNodeCount(Node* Head)
+{
+    int Count = 0;
+    Node* Current = Head;
+
+    while(Current != NULL)
+    {
+        Current = Current->NextNode;
+        Count++;
+    }
+
+    return Count;
+}
+
+
